1f8a582b4be6bb02abe73577998d8290bd1bfe8f
anxillo <andrea.a.giro@gmail.com>
2014-09-25 22:52:57 +0200

    temporizzato lo scavo

--- main.js
+++ main.js
@@ -7,14 +7,6 @@ var mainState = {
 preload: function() { 
     // Load the image 
     game.load.image('player', 'assets/player.png');
-    game.load.image('wallV', 'assets/wallVertical.png'); 
-    game.load.image('wallH', 'assets/wallHorizontal.png');
-    game.load.image('coin', 'assets/coin.png');
-    game.load.image('enemy', 'assets/enemy.png');
-
-
-
-
 },
 create: function() { 
     // Display the image on the screen 
@@ -88,116 +80,7 @@ update: function() {
     game.physics.arcade.overlap(this.player, this.enemies, this.playerDie, null, this);
 
 
-},
-    
-movePlayer: function() { 
-    // If the left arrow key is pressed
-    if (this.cursor.left.isDown) { 
-        // Move the player to the left 
-        this.player.body.velocity.x = -200; 
-    }
-    
-    // If the right arrow key is pressed 
-    else if (this.cursor.right.isDown) { 
-        // Move the player to the right 
-        this.player.body.velocity.x = 200; 
-    }
-    
-    // If neither the right or left arrow key is pressed 
-    else { 
-        // Stop the player 
-        this.player.body.velocity.x = 0; 
-    }
-    
-    // If the up arrow key is pressed and the player is touching the ground 
-    if (this.cursor.up.isDown && this.player.body.touching.down) { 
-        // Move the player upward (jump) 
-        this.player.body.velocity.y = -320; 
-    }
-    
-},
-
-createWorld: function() { 
-    // Create our wall group with Arcade physics 
-    this.walls = game.add.group(); this.walls.enableBody = true;
-    
-    // Create the 10 walls 
-    game.add.sprite(0, 0, 'wallV', 0, this.walls); // Left 
-    game.add.sprite(480, 0, 'wallV', 0, this.walls); // Right
-    game.add.sprite(0, 0, 'wallH', 0, this.walls); // Top left 
-    game.add.sprite(300, 0, 'wallH', 0, this.walls); // Top right 
-    game.add.sprite(0, 320, 'wallH', 0, this.walls); // Bottom left 
-    game.add.sprite(300, 320, 'wallH', 0, this.walls); // Bottom right
-    game.add.sprite(-100, 160, 'wallH', 0, this.walls); // Middle left 
-    game.add.sprite(400, 160, 'wallH', 0, this.walls); // Middle right
-    
-    var middleTop = game.add.sprite(100, 80, 'wallH', 0, this.walls); 
-    middleTop.scale.setTo(1.5, 1); 
-    
-    var middleBottom = game.add.sprite(100, 240, 'wallH', 0, this.walls); 
-    middleBottom.scale.setTo(1.5, 1);
-
-    // Set all the walls to be immovable 
-    this.walls.setAll('body.immovable', true);
-},
-
-playerDie: function() { 
-    game.state.start('main'); 
-},
-
-takeCoin: function(player, coin) { 
-    
-    // Increase the score by 5 
-    this.score += 5;
-
-    // Update the score label 
-    this.scoreLabel.text = 'score: ' + this.score;
-    
-    // Change the coin position 
-    this.updateCoinPosition();
-
-},
-
-updateCoinPosition: function() { 
-    // Store all the possible coin positions in an array 
-    var coinPosition = [ 
-        {x: 140, y: 60}, {x: 360, y: 60}, // Top row 
-        {x: 60, y: 140}, {x: 440, y: 140}, // Middle row 
-        {x: 130, y: 300}, {x: 370, y: 300} // Bottom row 
-    ];
-    
-    // Remove the current coin position from the array 
-    // Otherwise the coin could appear at the same spot twice in a row 
-    for (var i = 0; i < coinPosition.length; i++) { 
-        if (coinPosition[i].x === this.coin.x) { 
-            coinPosition.splice(i, 1);
-        }
-    }
-    
-    // Randomly select a position from the array 
-    var newPosition = coinPosition[ game.rnd.integerInRange(0, coinPosition.length-1)];
-    
-    // Set the new position of the coin 
-    this.coin.reset(newPosition.x, newPosition.y);
-},
-    
-addEnemy: function() { 
-    // Get the first dead enemy of the group 
-    var enemy = this.enemies.getFirstDead();
-    
-    // If there isn't any dead enemy, do nothing 
-    if (!enemy) { return; }
-    
-    // Initialise the enemy 
-    enemy.anchor.setTo(0.5, 1); 
-    enemy.reset(game.world.centerX, 0); 
-    enemy.body.gravity.y = 500; 
-    enemy.body.velocity.x = 100 * Phaser.Math.randomSign(); 
-    enemy.body.bounce.x = 1; 
-    enemy.checkWorldBounds = true; 
-    enemy.outOfBoundsKill = true;
-},
-
+}
 
 };
 
